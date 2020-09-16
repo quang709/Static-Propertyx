817171e157a16d8603ed54de9e04268f0f2838a4
quang709 <69231857+quang709@users.noreply.github.com>
2020-09-16 11:53:18 +0700

    Static-Property

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Static_Property
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Car car1 = new Car("Mazda 3", "Skyactiv 3");
+            Console.WriteLine( Car.numberOfCars);
+            Car car2 = new Car("Mazda 6", "Skyactiv 6");
+            Console.WriteLine(Car.numberOfCars); // numberOfCars goi tu class
+            Console.WriteLine(car1.getEngine());
+            Console.WriteLine(car1.getName());
+            Console.WriteLine(car2.getEngine());
+            Console.WriteLine(car2.getName());
+            Console.ReadLine();
+
+        }
+    }
+}
